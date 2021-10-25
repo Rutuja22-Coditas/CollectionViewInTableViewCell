26c244d7fe6d7dedd664f2de84d656ef7a62cbe0
Rutuja Hulsure <rutuja.hulsure@coditas.com>
2021-10-25 14:08:04 +0530

    Initial Commit

---
+++ CollectionViewInTableViewCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionViewInTableViewCell
+//
+//  Created by Coditas on 25/10/21.
+//  Copyright © 2021 Coditas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
